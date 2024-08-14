84b538742247de813575a6ed94d32f93ca06ece1
hexNulgath <machadoignacio@hotmail.com>
2024-08-14 21:03:13 +0000

    handeled memory leak with signal

--- shell.c
+++ shell.c
@@ -5,13 +5,24 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+char *buffer = NULL;
+void handle_sigint(int signal)
+{
+	free(buffer);
+	exit(0);
+}
 int main(void)
 {
 	ssize_t buffsize = 1024;
-	char *buffer = NULL, *path, *arguments;
+	char *path, *arguments;
 	int status, i;
 	char *argv[1024];
 	struct stat st;
+
+	buffer = malloc(sizeof(char) * buffsize);
+	if (buffer == NULL)
+		return(-1);
+	signal(SIGINT, handle_sigint);
 
 	printf("$ ");
 	while (getline(&buffer, &buffsize, stdin) != -1)
@@ -29,9 +40,13 @@ int main(void)
 			argv[i] = NULL;
 			pid_t p = fork();
 			if (p == 0)
-				execv(path, argv);
+			{
+				execv(argv[0], argv);
+			}
 			else
+			{
 				wait(&status);
+			}
 		}
 		printf("$ ");
 	}
